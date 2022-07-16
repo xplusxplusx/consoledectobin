8a3830dc1f73fee4eaeda2ed14541cf0fc282b42
xplusxplusx <whtcrck@gmail.com>
2022-07-16 19:55:50 +0300

    add all files

---
+++ sourcecode.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int a;
+	bool isActive = true;
+	string newn_r, newn;
+	string ans = "";
+
+	while (isActive) {
+		newn_r = "";
+		newn = "";
+		cout << "Enter a decimal number: ";
+
+		cin >> a;
+		while (a >= 1) {
+			newn_r += to_string(a % 2);
+			a /= 2;
+		}
+
+		for (int i = newn_r.length() - 1; i >= 0; i--) {
+			newn += newn_r[i];
+		}
+
+		cout << "Your binary number is " << newn << endl;
+		cout << "Continue? [Y/N] ";
+		cin >> ans;
+		if (ans == "n" || ans == "N")
+			isActive = false;
+	}
+
+	return 0;
+}
